6e39dc0200e5d499d4a1c06cd6b30a512e0c0e45
Amrito Dey <amritodey07@gmail.com>
2025-01-15 23:24:34 +0600

    almost complete

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Countries from './components/countries/countries'
+
+function App() {
+
+
+  return (
+    <div>
+      <h1>React world tour</h1>
+      <Countries></Countries>
+    </div>
+  )
+}
+
+export default App;
